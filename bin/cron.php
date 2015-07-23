911632ef7e5ee47cbc46a55b951dd57a97e51816
Finjon Kiang <kiange@gmail.com>
2015-07-23 13:43:14 +0800

    cron script

---
+++ bin/cron.php
@@ -0,0 +1,14 @@
+<?php
+
+$path = dirname(__DIR__);
+$now = date('Y-m-d H:i:s');
+
+exec("/usr/bin/php -q {$path}/reports.php");
+
+exec("cd {$path} && /usr/bin/git pull");
+
+exec("cd {$path} && /usr/bin/git add -A");
+
+exec("cd {$path} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'update datasets @ {$now}'");
+
+exec("cd {$path} && /usr/bin/git push origin master");
